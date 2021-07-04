9ce53e9ffa7e7d3a403e878fb4c3f8e1709baf0e
Mateusz Grube <s20156@pjwstk.edu.pl>
2021-07-04 14:08:32 +0200

    Initial commit

---
+++ src/main/java/jaz/s20156/jazs20156nbp/JazS20156NbpApplication.java
@@ -0,0 +1,13 @@
+package jaz.s20156.jazs20156nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS20156NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS20156NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jaz/s20156/jazs20156nbp/JazS20156NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package jaz.s20156.jazs20156nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS20156NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
